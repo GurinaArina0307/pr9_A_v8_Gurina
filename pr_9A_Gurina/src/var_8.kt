84655cc78746d8e7fe4fd2354f176a91910edc01
GurinaArina0307 <gurinaarina227@gmail.com>
2025-03-07 13:14:18 +0500

    Add files via upload

---
+++ pr_9A_Gurina/src/var_8.kt
@@ -0,0 +1,81 @@
+// базовый класс
+open class Samolet(var marka: String,
+                   var punktOtprav: String,
+                   var punktPrib: String,
+                   var data: String,
+                   var timeOtpr: String,
+                   var timePrib: String
+                   ){
+
+            open fun info(){
+                println("\nМодель самолета : $marka")
+                println("Дата отправления : $data")
+                println("Пункт отправления : $punktOtprav \nВремя : $timeOtpr")
+                println("Пункт прибытия : $punktPrib \nВремя : $timePrib")
+            }
+}
+//класс-наследник с дополнительным св-ом
+class GrusSamolet(
+    marka: String,
+    punktOtprav: String,
+    punktPrib: String,
+    data: String,
+    timeOtpr: String,
+    timePrib: String,
+    var massaGruza: Int // дополнительное св-во
+): Samolet(marka, punktOtprav, punktPrib, data, timeOtpr, timePrib){
+    open fun gruz(){
+        println("Грузоподъемность самолета : $massaGruza кг")
+    }
+}
+// второй класс-наследник с дополнительным св-ом
+class PasSamolet(
+    marka: String,
+    punktOtprav: String,
+    punktPrib: String,
+    data: String,
+    timeOtpr: String,
+    timePrib: String,
+    var countMest: Int // дополнительное св-во
+): Samolet(marka, punktOtprav, punktPrib, data, timeOtpr, timePrib){
+    open fun gruz(){
+        println("Кол-во мест : $countMest кг")
+    }
+}
+// класс с одинаковыми св-ми исп. как псевдоним
+class SamoletPsev1(
+    marka: String,
+    punktOtprav: String,
+    punktPrib: String,
+    data: String,
+    timeOtpr: String,
+    timePrib: String
+): Samolet(marka, punktOtprav, punktPrib, data, timeOtpr, timePrib)
+// второй класс с одинаковыми св-ми исп. как псевдоним
+class SamoletPsev2(
+    marka: String,
+    punktOtprav: String,
+    punktPrib: String,
+    data: String,
+    timeOtpr: String,
+    timePrib: String
+): Samolet(marka, punktOtprav, punktPrib, data, timeOtpr, timePrib)
+
+fun main(){
+    // объект базового класса
+    var plane1 = Samolet("Boing 312", "Moscow", "Ekaterinburg", "08.03.2024", "16.40", "19.20")
+    plane1.info()
+
+    var gruzPlane1 = GrusSamolet("Air 365", "London", "Paris", "08.03.2024", "10.40", "14.00",236)
+    gruzPlane1.info()
+
+    var pasPlane1 = PasSamolet("Boing 312", "Sochi", "Samara", "09.03.2024", "1.40", "2.20", 240)
+    plane1.info()
+
+    var psevPlane1 = SamoletPsev1("Boing 312", "Rostov", "Ekaterinburg", "08.03.2024", "22.05", "00.55")
+    plane1.info()
+
+    var psevPlane2 = SamoletPsev2("Boing 1", "Moscow", "Sankt-Peterburg", "09.03.2024", "6.10", "7.30")
+    plane1.info()
+
+}
